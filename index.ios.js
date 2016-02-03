31df9329d0b4779caea717935979cd37d9aa3451
Charlie Blackstock <charlieblackstock@gmail.com>
2016-02-03 15:29:04 -0700

    got working timer on start with formating

--- index.ios.js
+++ index.ios.js
@@ -1,18 +1,25 @@
+var formatTime = require('minutes-seconds-milliseconds');
 var React = require('react-native');
 var {
     Text,
     View,
+    TouchableHighlight,
     AppRegistry,
     StyleSheet
 } = React;
 
 var StopWatch = React.createClass({
+    getInitialState: function() {
+        return {
+            timeElapsed: null
+        }
+    },
     render: function() {
         return <View style={styles.container}>
             <View style={[styles.header, this.border('yellow')]}>
                 <View style={[styles.timerWrapper, this.border('red')]}>
                     <Text>
-                        00:00.0
+                        {formatTime(this.state.timeElapsed)}
                     </Text>
                 </View>
                 <View style={[styles.buttonWrapper, this.border('green')]}>
@@ -29,11 +36,14 @@ var StopWatch = React.createClass({
         </View>
     },
     startStopButton: function() {
-        return <View>
+        return <TouchableHighlight
+            underlayColor="gray"
+            onPress={this.onStartPress}
+            >
             <Text>
                 Start
             </Text>
-        </View>
+        </TouchableHighlight>
     },
     lapButton: function() {
         return <View>
@@ -41,6 +51,15 @@ var StopWatch = React.createClass({
                 Lap
             </Text>
         </View>
+    },
+    onStartPress: function() {
+        var startTime = new Date();
+
+        setInterval(() => {
+            this.setState({
+                timeElapsed: new Date() - startTime
+            });
+        }, 30);
     },
     border: function(color) {
         return {
@@ -63,11 +82,16 @@ var styles = StyleSheet.create({
     },
     timerWrapper: { // red
         // takes up 5/8ths of avail space since button and timer are siblings
-        flex: 5
+        flex: 5,
+        justifyContent: 'center',
+        alignItems: 'center'
     },
     buttonWrapper: { // green
         // takes up 3/8ths of avail space
-        flex: 3
+        flex: 3,
+        flexDirection: 'row',
+        justifyContent: 'space-around',
+        alignItems: 'center'
     }
 });
 
